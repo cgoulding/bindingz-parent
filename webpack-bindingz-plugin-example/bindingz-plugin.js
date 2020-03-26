eb6878aa039ecc50eadb6b12a371f26e111e571f
Connor Goulding <connorg_2000@hotmail.com>
2020-03-26 21:16:26 +0000

    Indendation

--- webpack-bindingz-plugin-example/bindingz-plugin.js
+++ webpack-bindingz-plugin-example/bindingz-plugin.js
@@ -8,16 +8,16 @@ class BindingzPlugin {
     apply(compiler) {
         compiler.hooks.beforeCompile.tap('Process resources', (stats) => {
             var xmlHttp = new XMLHttpRequest.XMLHttpRequest();
-        xmlHttp.open("GET", 'http://localhost:8080/api/v1/schemas/asdf/asdf', false); // false for synchronous request
-        xmlHttp.send(null);
-        var responseText = xmlHttp.responseText;
-        Fs.writeFileSync('response.json', responseText, 'utf8')
+            xmlHttp.open("GET", 'http://localhost:8080/api/v1/schemas/asdf/asdf', false); // false for synchronous request
+            xmlHttp.send(null);
+            var responseText = xmlHttp.responseText;
+            Fs.writeFileSync('response.json', responseText, 'utf8')
 
-        var response = JSON.parse(responseText);
-        JsonSchemaToTypescript.compile(response.content.schema, 'woopwoop', {bannerComment: bannerComment}).then(r => {
-            Fs.writeFileSync('fun.ts', r, 'utf8')
+            var response = JSON.parse(responseText);
+            JsonSchemaToTypescript.compile(response.content.schema, 'woopwoop', {bannerComment: bannerComment}).then(r => {
+                Fs.writeFileSync('fun.ts', r, 'utf8')
+            });
         });
-    });
     }
 }
 
